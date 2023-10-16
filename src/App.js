240cddc1840485f5fc51cc2e5aa9ac1b20607500
laco-andrea <a.lacommara@studenti.unisa.it>
2023-10-16 03:09:35 +0200

    [IMP] aggiunte nuove componenti ecorretto app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { ContactForm } from './ui-components';
+import { ContactUs } from './ui-components';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ContactForm
-          onSubmit={fields => {
-          /* Handle form submission */
-          }}
-        />
+        <ContactUs />
       </header>
     </div>
   );
